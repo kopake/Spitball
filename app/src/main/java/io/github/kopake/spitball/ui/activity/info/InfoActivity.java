936a14fbbada098f16d6e8d94b94ed74c2e646f6
Kole <kopake17@gmail.com>
2025-02-09 09:07:22 -0600

    Reworked info screen
    Also updated font of team name displays in settings menu

--- app/src/main/java/io/github/kopake/spitball/ui/activity/info/InfoActivity.java
+++ app/src/main/java/io/github/kopake/spitball/ui/activity/info/InfoActivity.java
@@ -1,14 +1,18 @@
 package io.github.kopake.spitball.ui.activity.info;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.HapticFeedbackConstants;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
 
 import io.github.kopake.spitball.R;
+import io.github.kopake.spitball.file.FileSystemUtilities;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -17,6 +21,8 @@ public class InfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
         hideNavigationBar();
+
+        setCustomWordListsDirectoryValue();
     }
 
     private void hideNavigationBar() {
@@ -25,6 +31,16 @@ public class InfoActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
         WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView()).setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+    }
+
+    private void setCustomWordListsDirectoryValue() {
+        TextView textView = findViewById(R.id.customWordListsValueTextView);
+        textView.setText(FileSystemUtilities.getCustomWordListsDirectory().getAbsolutePath());
+    }
+
+    public void onOpenRulesButtonClick(View view) {
+        view.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+        openRules();
     }
 
     public void onBackButtonClick(View view) {
@@ -40,5 +56,11 @@ public class InfoActivity extends AppCompatActivity {
     private void goBackToMainMenu() {
         finish();
         overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
+    private void openRules() {
+        Uri uri = Uri.parse("https://docs.google.com/viewer?url=https://raw.githubusercontent.com/kopake/Spitball/main/app/src/main/assets/spitball_rules.pdf");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 }
